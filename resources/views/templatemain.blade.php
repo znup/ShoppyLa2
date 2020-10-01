38905fb482dfaebeccfe90c7e7bccac0df091f9a
Adan Pelcastre Roblero <znup.kreyz.vlc@gmail.com>
2020-09-30 22:42:33 -0700

    add archives

---
+++ resources/views/templatemain.blade.php
@@ -0,0 +1,287 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IEdge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="ShopLaravel">
+    <meta name="keyword" content="Shop Laravel 2">
+    <title>ShoppyLa2</title>
+    <!-- Icons -->
+    <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{asset('css/simple-line-icons.min.css')}}" rel="stylesheet">
+    <!-- Main styles for this application -->
+    <link href="{{asset('css/style.css')}}" rel="stylesheet">
+</head>
+
+<body class="app header-fixed sidebar-fixed aside-menu-fixed aside-menu-hidden">
+<header class="app-header navbar">
+        <button class="navbar-toggler mobile-sidebar-toggler d-lg-none mr-auto" type="button">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <!--PONER LOGO-->
+        <!--<a class="navbar-brand" href="#"></a>-->
+        <button class="navbar-toggler sidebar-toggler d-md-down-none" type="button">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <ul class="nav navbar-nav d-md-down-none">
+            <li class="nav-item px-3">
+                <a class="nav-link" href="#">Dashbord</a>
+            </li>
+           
+        </ul>
+        <ul class="nav navbar-nav ml-auto">
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                    <img src="{{asset('img/avatars/6.jpg')}}" class="img-avatar" alt="admin@bomuca.com">
+                    <span class="d-md-down-none">usuario </span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right">
+                    <div class="dropdown-header text-center">
+                        <strong>Cuenta</strong>
+                    </div>
+                    <a class="dropdown-item" href="" 
+                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    <i class="fa fa-lock"></i> Cerrar sesión</a>
+
+                    <form id="logout-form" action="" method="POST" style="display: none;">
+                      
+                    </form>
+                </div>
+            </li>
+        </ul>
+    </header>
+
+    <div class="app-body">
+
+       <div class="sidebar">
+            <nav class="sidebar-nav">
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#"><i class="icon-speedometer"></i> Dashbord</a>
+                    </li>
+                    <li class="nav-title">
+                        Menú
+                    </li>
+
+                   
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fa fa-list"></i> Categorías</a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fa fa-tasks"></i> Productos</a>
+                    </li>
+                      
+            
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fa fa-shopping-cart"></i> Compras</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fa fa-users"></i> Proveedores</a>
+                    </li>
+                       
+                   
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fa fa-suitcase"></i> Ventas</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fa fa-users"></i> Clientes</a>
+                    </li>
+                        
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fa fa-user"></i> Usuarios</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fa fa-list"></i> Roles</a>
+                    </li>
+                        
+                    
+                </ul>
+            </nav>
+            <button class="sidebar-minimizer brand-minimizer" type="button"></button>
+        </div>
+
+        <!-- Contenido Principal -->
+       <main class="main">
+            <!-- Breadcrumb -->
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item active"><a href="/">BACKEND - SHOPPILA2</a></li>
+            </ol>
+            <div class="container-fluid">
+                <!-- Ejemplo de tabla Listado -->
+                <div class="card">
+                    <div class="card-header">
+
+                       <h2>Listado de Categorías</h2><br/>
+                      
+                        <button class="btn btn-primary btn-lg" type="button" data-toggle="modal" data-target="#abrirmodal">
+                            <i class="fa fa-plus fa-2x"></i>&nbsp;&nbsp;Agregar Categoría
+                        </button>
+                    </div>
+                    <div class="card-body">
+                        <div class="form-group row">
+                            <div class="col-md-6">
+                                <div class="input-group">
+                                    <select class="form-control col-md-3">
+                            <option value="nombre">Categoría</option>
+                                      <option value="descripcion">Descripción</option>
+                                    </select>
+                                    <input type="text" class="form-control" placeholder="Buscar texto">
+                                    <button type="submit"  class="btn btn-primary"><i class="fa fa-search"></i> Buscar</button>
+                                </div>
+                            </div>
+                        </div>
+                        <table class="table table-bordered table-striped table-sm">
+                            <thead>
+                                <tr class="bg-primary">
+                                   
+                                    <th>Categoría</th>
+                                    <th>Descripción</th>
+                                    <th>Estado</th>
+                                    <th>Editar</th>
+                                    <th>Cambiar Estado</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                               
+                                <tr>
+                                    
+                                    <td>categoria</td>
+                                    <td>descripcion</td>
+                                    <td>
+                                        <button type="button" class="btn btn-success btn-md">
+                                    
+                                          <i class="fa fa-check fa-2x"></i> Activo
+                                        </button>
+                                       
+                                    </td>
+
+                                    <td>
+                                        <button type="button" class="btn btn-info btn-md" data-toggle="modal" data-target="#abrirmodal">
+
+                                          <i class="fa fa-edit fa-2x"></i> Editar
+                                        </button> &nbsp;
+                                    </td>
+
+                                    <td>
+
+                                        
+                                        <button type="button" class="btn btn-danger btn-sm">
+                                            <i class="fa fa-lock fa-2x"></i> Desactivar
+                                        </button>
+                                       
+                                    </td>
+                                </tr>
+                               
+                            </tbody>
+                        </table>
+                        <nav>
+                            <ul class="pagination">
+                                <li class="page-item">
+                                    <a class="page-link" href="#">Anterior</a>
+                                </li>
+                                <li class="page-item active">
+                                    <a class="page-link" href="#">1</a>
+                                </li>
+                                <li class="page-item">
+                                    <a class="page-link" href="#">2</a>
+                                </li>
+                                <li class="page-item">
+                                    <a class="page-link" href="#">3</a>
+                                </li>
+                                <li class="page-item">
+                                    <a class="page-link" href="#">4</a>
+                                </li>
+                               
+                                <li class="page-item">
+                                    <a class="page-link" href="#">Siguiente</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <!-- Fin ejemplo de tabla Listado -->
+            </div>
+            <!--Inicio del modal agregar/actualizar-->
+            <div class="modal fade" id="abrirmodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" style="display: none;" aria-hidden="true">
+                <div class="modal-dialog modal-primary modal-lg" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title">Agregar categoría</h4>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">×</span>
+                            </button>
+                        </div>
+                       
+                        <div class="modal-body">
+                            
+                            <div class="form-group row div-error">
+                                
+                                <div class="text-center text-error">
+                                    
+                                    <div></div>
+
+                                </div>
+                            
+                            </div>
+                             
+
+                            <form action="" method="post" enctype="multipart/form-data" class="form-horizontal">
+                                <div class="form-group row">
+                                    <label class="col-md-3 form-control-label" for="text-input">Categoría</label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control" placeholder="Nombre de categoría">
+                                       
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-3 form-control-label" for="email-input">Descripción</label>
+                                    <div class="col-md-9">
+                                    <input type="email" class="form-control" placeholder="Ingrese descripcion">
+                                    </div>
+                                </div>
+
+
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fa fa-times fa-2x"></i> Cerrar</button>
+                            <button type="button" class="btn btn-success"><i class="fa fa-save fa-2x"></i> Guardar</button>
+                           
+                        </div>
+                    </div>
+                    <!-- /.modal-content -->
+                </div>
+                <!-- /.modal-dialog -->
+            </div>
+            <!--Fin del modal-->
+           
+            
+        </main>
+        <!-- /Fin del contenido principal -->
+    </div>   
+
+    <footer class="app-footer">
+        <span><a href="#">Todos los derechos reservados!! </a> &copy; 2099</span>
+        <span class="ml-auto">Desarrollado por <a href="">Adan P.R.</a></span>
+    </footer>
+
+    <!-- Bootstrap and necessary plugins -->
+    <script src="{{asset('js/jquery.min.js')}}"></script>
+    <script src="{{asset('js/popper.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('js/pace.min.js')}}"></script>
+    <!-- Plugins and scripts required by all views -->
+    <script src="{{asset('js/Chart.min.js')}}"></script>
+    <!-- GenesisUI main scripts -->
+    <script src="{{asset('js/template.js')}}"></script>
+</body>
+
+</html>
