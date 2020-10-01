f20f9a292e5fb4cb9a21bddb7b8679b4c9c7f44a
unknown <znup.kreyz.vlc@gmail.com>
2020-10-01 10:51:13 -0700

    add templates

---
+++ resources/views/templates/sidebar.blade.php
@@ -0,0 +1,51 @@
+<div class="sidebar">
+    <nav class="sidebar-nav">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="#"><i class="icon-speedometer"></i> Dashbord</a>
+            </li>
+            <li class="nav-title">
+                Menú
+            </li>
+
+
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fa fa-list"></i> Categorías</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fa fa-tasks"></i> Productos</a>
+            </li>
+
+
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fa fa-shopping-cart"></i> Compras</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fa fa-users"></i> Proveedores</a>
+            </li>
+
+
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fa fa-suitcase"></i> Ventas</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fa fa-users"></i> Clientes</a>
+            </li>
+
+
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fa fa-user"></i> Usuarios</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fa fa-list"></i> Roles</a>
+            </li>
+
+
+        </ul>
+    </nav>
+    <button class="sidebar-minimizer brand-minimizer" type="button"></button>
+</div>
